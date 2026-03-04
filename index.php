d1f52fb6104d7dd09a618f004f52c8248e2c242c
JohnDima1 <timenshane@gmail.com>
2026-03-04 12:38:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ try {
   <h2>Movies</h2>
 
   <ul class="movie-list">
+<<<<<<< Updated upstream
     <li>
       <img src="images/logo2.png" alt="Movie 1">
       <span>Jibril, How to do nothing</span>
@@ -54,6 +55,37 @@ try {
       <img src="images/logo2.png" alt="Movie 4">
       <span>Hentai vs furries the movie</span>
     </li>
+=======
+    <?php
+    try {
+      $sql = "SELECT id, name, video FROM video";
+      $result = $conn->query($sql);
+
+      if ($result->rowCount() > 0) {
+        // The loop starts here
+        while($row = $result->fetch()) {
+          ?>
+            <a href="videopagina.php?id=<?php echo $row['id']; ?>">
+              <li>
+              <img src="images/logo2.png" alt="Movie Logo">
+              <div class="movie-info">
+                <span class="label">FILMNAAM</span>
+                <span class="name"><?php echo $row['name']; ?></span>
+              </div>
+              </li>
+            </a>
+          
+          <?php
+        }
+      } else {
+        echo "No records found.";
+      }
+    } catch(PDOException $e) {
+      echo "Error: " . $e->getMessage();
+    }
+    $conn = null;
+    ?>
+>>>>>>> Stashed changes
   </ul>
 </main>
 
